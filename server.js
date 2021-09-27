676b681ffa47ac749f60ca3be8298e159138cbde
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-09-27 19:05:54 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static(`./dist/chart-automation`));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/employee-chart'));
 
-app.get(`/*`, function(req, res) {
-  res.sendFile(`index.html`, {root: `dist/chart-automation/`}
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/employee-chart/'}),
 );
-});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
