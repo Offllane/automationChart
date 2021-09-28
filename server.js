1ddf16199bdf327f37954d8cf51d0ba94cc63297
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-09-28 08:21:08 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/employee-chart'));
+app.use(express.static('./dist/chart-automation'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/employee-chart/'}),
+  res.sendFile('index.html', {root: 'dist/chart-automation/'}),
 );
 
 // Start the app by listening on the default Heroku port
