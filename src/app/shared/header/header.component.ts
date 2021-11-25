21f2375108a502341c8e6ba23d1a08b5f9b6f311
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-11-25 17:59:12 +0300

    add buffer-item-counter

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,27 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {HomeService} from "../../home/home.service";
 import {Switch, TreeType} from "../../models/types";
 import {Router} from "@angular/router";
+import {ChartService} from "../../home/components/chart/chart.service";
+import {Subscription} from "rxjs";
+import {listChartItem} from "../../models/interfaces";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private dataSubscription: Subscription = new Subscription();
+  public bufferItemCounter: number = 0;
   public treeType: TreeType = 'horizontal';
   public bufferState: Switch = 'close';
   @Input() mode = 'home';
 
   constructor(
     private homeService: HomeService,
+    private chartService: ChartService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.homeService.treeType.subscribe((data: TreeType) => {
+    this.dataSubscription.add(this.homeService.treeType.subscribe((data: TreeType) => {
       this.treeType = data;
-    })
+    }));
+    this.dataSubscription.add(this.chartService.bufferListChartData.subscribe((data: Array<listChartItem>) => {
+      this.bufferItemCounter = data.length;
+    }));
   }
 
   public switchTreeType(treeType: TreeType) {
@@ -39,5 +48,9 @@ export class HeaderComponent implements OnInit {
   public toggleBuffer(): void {
     this.bufferState = this.bufferState === 'open' ? 'close' : 'open';
     this.homeService.bufferState.next(this.bufferState);
+  }
+
+  ngOnDestroy(): void {
+    this.dataSubscription.unsubscribe();
   }
 }
